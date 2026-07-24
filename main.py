05f17048d26f61a2cd062de642ca0df7f31cdc0b
Julio Higor <juliohigordemattos@gmail.com>
2026-07-24 17:13:31 -0300

    Refatora operações usando encontrar_livro

--- main.py
+++ main.py
@@ -8,27 +8,28 @@ def mostrar_menu():
     print("3 - Buscar Livro")
     print("4 - remover Livro")
     print("5 - editar Livro")
-    print("8 - Sair")
+    print("6 - Sair")
     print(" ")
 
-livros = []
-while True:
-    mostrar_menu()
-       
-    escolha = int(input("Escolha uma opçâo: "))
+def encontrar_livro(livro, buscar):
+    for livro in livros:
+        if buscar.lower() in livro["titulo"].lower():
+            return livro
 
-    if escolha == 1:
-        print("1 - Cadastrando Livro")
-        nome = input("qual livro deseja cadastrar ?: ")
-        ano = int(input("ano: "))
-        autor = input("Autor: ")
-        livro = {
-            "titulo": nome,
-            "autor": autor,
-            "ano": ano }
-        livros.append(livro)
-    
-    elif escolha == 2:
+    return None
+
+def cadastrar_livro(livros):
+    print("1 - Cadastrando Livro")
+    nome = input("qual livro deseja cadastrar ?: ")
+    ano = int(input("ano: "))
+    autor = input("Autor: ")
+    livro = {
+        "titulo": nome,
+        "autor": autor,
+        "ano": ano }
+    livros.append(livro)
+
+def listar_livros(livros):
         print("===== LIVROS CADASTRADOS =====")
         if livros:
             for livro in livros:
@@ -39,60 +40,71 @@ while True:
                 
         else:
             print("sem livros cadastrados")
+
+def buscar_livro(livros):
+    buscar = input("Digite o nome do livro: ").strip()
+
+    livro = encontrar_livro(livros, buscar)
+        
+    if livro:
+        print("Livro encontrado!")
+        print("Título:", livro["titulo"])
+        print("Autor:", livro["autor"])
+        print("Ano:", livro["ano"])                    
+        print()
+
+    else:
+        print("Livro não encontrado.")
+
+def remover_livro(livros):
+    buscar = input("Digite o nome do livro: ").strip()
+
+    livro = encontrar_livro(livros, buscar)
+
+    if livro:
+        livros.remove(livro)
+        print("Livro removido com sucesso!")
+    else:
+        print("Livro não encontrado.")
+
+def editar_livro(livros):
+    buscar = input("Digite o nome do livro: ").strip()
+
+    livro = encontrar_livro(livros, buscar)
+
+    if livro:
+        livro["titulo"] = input("Novo título: ")
+        livro["autor"] = input("Novo autor: ")
+        livro["ano"] = int(input("Novo ano: "))
+        print("Livro editado com sucesso!")
+    else:
+        print("Livro não encontrado.")
+
+livros = []
+while True:
+    mostrar_menu()
+
+    escolha = int(input("Escolha uma opçâo: "))
+
+    if escolha == 1:
+        cadastrar_livro(livros)
+    
+    elif escolha == 2:
+        listar_livros(livros)
        
     elif escolha == 3:
-        buscar = input("Digite o nome do livro: ").strip()
-        encontrado = False
-
-        for livro in livros:
-            if buscar.lower() in livro["titulo"].lower():
-                print("Livro encontrado!")
-                print("Título:", livro["titulo"])
-                print("Autor:", livro["autor"])
-                print("Ano:", livro["ano"])                    
-                print()
-
-                encontrado = True
-                break
-
-        if not encontrado:
-            print("Livro não encontrado.")
+        buscar_livro(livros)
 
     elif escolha ==4:
-        buscar = input("Digite o nome do livro: ").strip()
-        encontrado = False
-
-        for livro in livros:
-            if buscar.lower() in livro["titulo"].lower():
-                livros.remove(livro)
-                print("Livro removido com sucesso!")
-
-                encontrado = True
-                break
-
-        if not encontrado: 
-            print("livro não encontrado")
+        remover_livro(livros)
 
     elif escolha ==5:
-        buscar = input("Digite o nome do livro: ").strip()
-        encontrado = False
+        editar_livro(livros)   
 
-        for livro in livros:
-             if buscar.lower() in livro["titulo"].lower():
-                livro["titulo"] = input("Novo Titulo: ")
-                livro["autor"] = input("Novo Autor: ")
-                livro["ano"] = int(input("Novo Ano: "))
-                print("Livro editado com sucesso!")
-
-                encontrado = True
-                break    
-
-        if not encontrado: 
-            print("livro não encontrado")     
-
-    elif escolha ==8:
-        print("8 - Encerrando Sistema")
+    elif escolha ==6:
+        print("6 - Encerrando Sistema")
         break
     
     else:
         print("Código inválido")
+        
